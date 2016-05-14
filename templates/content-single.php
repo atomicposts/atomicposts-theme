e9f7563e9637c7b488446fd014f0ad172f117dd5
Galan <juanjogalanlopez@gmail.com>
2016-05-14 12:44:24 +0200

    Ajustes estilos

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,10 +3,11 @@
     <header>
         <?php echo get_avatar( $post->post_author, 46 ); ?>
         <?php get_template_part('templates/entry-meta'); ?>
+        <?php the_post_thumbnail('full', array('class' => 'img-responsive')); ?>
         <h1 class="entry-title"><?php the_title(); ?></h1>
     </header>
     <div class="entry-content">
-      <?php the_content(); ?>
+        <?php the_content(); ?>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,4 +1,8 @@
 <div class="image clearfix">
-    <img class="img-responsive" src="<?= get_template_directory_uri();?>/assets/images/liquidators.svg" alt="<?= get_bloginfo("name"); ?>"/>
+    <div class="row">
+        <div class="col-xs-9 col-xs-offset-3">
+            <img class="img-responsive" src="<?= get_template_directory_uri();?>/assets/images/liquidators.svg" alt="<?= get_bloginfo("name"); ?>"/>
+        </div>
+    </div>
 </div>
 <?php dynamic_sidebar('sidebar-primary'); ?>
