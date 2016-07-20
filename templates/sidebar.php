f62c64712ef91ba07be3f587b96dfd625db4d641
Galan <juanjogalanlopez@gmail.com>
2016-07-20 23:49:43 +0200

    Ajuste mensaje de cookies, twitter en sidebar, ajuste footer con mensaje de cookies

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -4,5 +4,12 @@
             <img class="img-responsive" src="<?= get_template_directory_uri();?>/dist/images/liquidators.svg" alt="<?= get_bloginfo("name"); ?>"/>
         </div>
     </div>
+    <div class="row">
+        <div class="col-xs-2 pull-right">
+            <a href="http://twitter.com/atomicposts" target="_blank" id="twitter-link">
+                <img class="img-responsive" src="<?= get_template_directory_uri();?>/dist/images/twitter.svg" alt="<?= get_bloginfo("name"); ?>"/>
+            </a>
+        </div>
+    </div>
 </div>
 <?php dynamic_sidebar('sidebar-primary'); ?>
