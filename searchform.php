8487aefb3d71fb1a45e0e6f9650b79ff6898bb6e
Galan <juanjogalanlopez@gmail.com>
2016-07-23 19:08:22 +0200

    Arreglo formulario búsqueda y estilo de título de entrada

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <div class="search-box">
     <form method="get" id="searchform" action="<?php echo home_url(); ?>/">
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="Buscar...">
+            <input type="text" class="form-control" placeholder="Buscar..." value="<?php the_search_query(); ?>" name="s" id="s" />
             <span class="input-group-btn">
-                <button class="btn btn-primary" type="button">Ir</button>
+                <button class="btn btn-primary" type="submit">Ir</button>
             </span>
         </div>
         <!-- /input-group -->
